cee3a0d9a59f0c101cbcf0575e3b2c1d67b1cca7
Terence1991 <cplterencehawes@gmail.com>
2019-01-18 16:27:42 +0000

    using state to query

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,20 +5,21 @@ import SearchBar from './SearchBar.js'
 
 
 class App extends  React.Component {
+   state = { images: []}
   async onSearchSubmit(term) {
-    const response = await axios
-    .get('https://api.unsplash.com/search/photos', {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {query: term},
       headers: {
         Authorization: "Client-ID 32560efc027a42b1c7f18b48b46f71cfbffc2a01f2f7891fa6af2846d4aceeb2"
       }
     });
-    console.log(response.data.results)
+    this.setState({images: response.data.results})
   }
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
       <SearchBar onSubmit={this.onSearchSubmit}/>
+      Found: {this.state.images.length} images 
       </div>
     )
   } 
