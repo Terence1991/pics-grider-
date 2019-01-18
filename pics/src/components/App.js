f124c9f8d804925a5233ccaffc73254e7d2e86d9
Terence1991 <cplterencehawes@gmail.com>
2019-01-18 03:43:15 +0000

    api request built

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -6,7 +6,12 @@ import SearchBar from './SearchBar.js'
 
 class App extends  React.Component {
   onSearchSubmit(term) {
-    console.log(term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: "Client-ID 32560efc027a42b1c7f18b48b46f71cfbffc2a01f2f7891fa6af2846d4aceeb2"
+      }
+    });
   }
   render() {
     return (
